2956e5e0c4d703c1f254ade40d382ada139e175c
Diego Lugo <contacto.dalo@gmail.com>
2024-03-25 22:43:18 -0600

    Two easy hr problems solved

---
+++ hackerank/easy_hr_mars_exploration.py
@@ -0,0 +1,14 @@
+def marsExploration(s):
+    changed_chars = 0
+    for i in range(0, len(s)):
+        if i % 3 == 2 or i % 3 == 0:
+            if s[i] != 'S':
+                changed_chars += 1
+        else:
+            if s[i] != 'O':
+                changed_chars += 1
+    return changed_chars
+
+if __name__ == '__main__':
+    print(marsExploration("SOSSPSSQSSOR"))
+    print(marsExploration("SOSSOSSOS"))
---
+++ hackerank/easy_hr_pangram.py
@@ -0,0 +1,10 @@
+def pangrams(s):
+    set_aux = set()
+    for i in range(0, len(s)):
+        if s[i].isalpha():
+            set_aux.add(s[i].lower())
+    return "pangram" if len(set_aux) == 26 else "not pangram"
+
+if __name__ == '__main__':
+    print(pangrams("We promptly judged antique ivory buckles for the next prize"))
+    print(pangrams("We promptly judged antique ivory buckles for the prize"))
