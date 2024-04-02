6de343b6acbd8ac84e2fa7c51094e417927a4a1e
Diego Lugo <diego.lugo@enginecore.com.mx>
2024-04-02 17:24:35 -0600

    Solved one easy leetcode problem

---
+++ leetcode/easy_lc383_ransom_note.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        for char in ransomNote:
+            if char not in magazine:
+                return False
+            else:
+                magazine = magazine.replace(char, '', 1)
+        return True
+
+if __name__ == '__main__':
+  print(Solution().canConstruct("aa", "ab"))
