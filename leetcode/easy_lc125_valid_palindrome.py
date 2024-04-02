84156a526220a9982b2c5ef1601bb61986b31b8c
Diego Lugo <diego.lugo@enginecore.com.mx>
2024-04-02 09:01:11 -0600

    Solved easy leetcode problem

---
+++ leetcode/easy_lc125_valid_palindrome.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join([char.lower() for char in s if char.isalnum()])
+        if len(s) != 0:
+            for i in range(0, len(s)):
+                if s[i] != s[-i-1]:
+                    return False
+        return True
+
+if __name__ == '__main__':
+  print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
