cfd8ee19453f224d5499580bc093601735d8c23b
DiegoLugo <contacto.dalo@gmail.com>
2024-04-05 10:39:28 -0600

    V2 of easy leetcode algorithms problem

---
+++ leetcode/easy_lc58_length_of_last_wordv2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+
+        string.split() with no separator splits the string
+        comboing any consecutive whitespaces and does
+        strip under the hood (ignores whitespaces at the beginning and at the
+        end of the string). If string is whitespaces only, returns []
+        """
+        return len(s.split()[-1])
+
+
+if __name__ == '__main__':
+    print(Solution().lengthOfLastWord("Hello World"))
+    # print(Solution().lengthOfLastWord("   fly me   to   the moon  "))
