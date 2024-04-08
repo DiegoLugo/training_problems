85b2a5e4b8175ad89ffac00c9c908715c6d0a9b6
DiegoLugo <contacto.dalo@gmail.com>
2024-04-08 09:32:28 -0600

    Solved one easy lc algorithms problem

---
+++ leetcode/easy_lc1700_number_of_students_unable_to_eat_lunch.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+        i = 0
+        while i < len(students):
+            if sandwiches[0] == students[0]:
+                sandwiches.pop(0)
+                students.pop(0)
+                i = 0
+            else:
+                students.append(students.pop(0))
+                i += 1
+        return len(students)
+
+
+if __name__ == '__main__':
+    print(Solution().countStudents([1,1,0,0]))
+    #print(Solution().countStudents([1,1,1,0,0,1]))
