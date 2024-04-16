f0e51ca7a4b4c89492ef3fc271981095332aab29
DiegoLugo <contacto.dalo@gmail.com>
2024-04-16 08:26:28 -0600

    Implemented queue ds

---
+++ ds/Queue.py
@@ -0,0 +1,38 @@
+class Node:
+    value: any
+    next: any
+
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+# FIFO
+class Queue:
+    __head: Node
+    __tail: Node
+    length: int
+
+    def __init__(self) -> None:
+        self.__head = None
+        self.__tail = None
+        self.length = 0
+
+    def enqueue(self, item: any) -> None:
+        new_node = Node(value=item)
+        self.length += 1
+        if not self.__tail:
+            self.__head = self.__tail = new_node
+        self.__tail.next = new_node
+        self.__tail = new_node
+
+    def deque(self) -> any | None:
+        if not self.__head:
+            return None
+        self.length -= 1
+        dequed = self.__head
+        dequed.next = None
+        self.__head = self.__head.next
+        return dequed.value
+
+    def peek(self) -> Node | None:
+        return self.head if self.head else None
