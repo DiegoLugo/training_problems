f16479998e75780ed576c1f76904997fce5a89e3
DiegoLugo <contacto.dalo@gmail.com>
2024-05-07 08:48:41 -0600

    style: File renaming

--- leetcode/medium_lc2816.py
+++ leetcode/medium_lc2816.py
@@ -1,9 +1,11 @@
+# 2816. Double a Number Represented as a Linked List
+
 # Definition for singly-linked list.
 class ListNode(object):
      def __init__(self, val=0, next=None):
          self.val = val
          self.next = next
-         
+
 class Solution(object):
     def doubleIt(self, head):
         """
